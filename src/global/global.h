2c9a8d32a12750519b2b9dbe068a89d58d9a9604
Guilherme Brogio Macedo da Silva <guilhermebrogio.ps@gmail.com>
2024-10-26 11:58:52 -0300

    Refactor global.h: Add administrator code and password constants
    - Added ADM_CODE and ADM_PASSWORD constants to global.h for administrator authentication.
    - Updated writeText() function signature to use a typedef for GenericType.
    - Added new functions: getInput(), printError(), clearFooter(), noValid(), confirm(), goTo(), cls(), and awaitPressAnyKey().
    
    Closes #issue-number

--- src/global/global.h
+++ src/global/global.h
@@ -3,9 +3,22 @@
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 24
 #define APP_NAME "G.B.M.S. Bank"
+#define ADM_CODE "123"
+#define ADM_PASSWORD "123"
 
-void writeText(char* text, int x, int y, int disableProporcion);
+typedef void *GenericType;
+
+void writeText(char *text, int x, int y, int disableProporcion);
 char command();
 void clearInputBuffer();
+void getInput(char mask[], int length, GenericType input, char err[], int x,
+              int y, int validation());
+void printError(char err[]);
+void clearFooter();
+int noValid();
+char confirm(char msg[]);
+void goTo(int x, int y);
+void cls();
+void awaitPressAnyKey();
 
 #endif
