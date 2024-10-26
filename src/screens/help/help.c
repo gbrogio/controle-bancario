0599408d35ca529bf1573ecfed4b6646b8465aa2
Guilherme Brogio Macedo da Silva <guilhermebrogio.ps@gmail.com>
2024-10-26 11:59:19 -0300

    Refatorar tela de ajuda: Atualizar texto e comandos disponíveis

--- src/screens/help/help.c
+++ src/screens/help/help.c
@@ -1,14 +1,14 @@
 #include "help.h"
 #include "../../global/global.h"
 #include "../../utils/screen/screen.h"
-#include "../../utils/cls/cls.h"
 
 void helpScreen() {
   cls();
-  writeText("HELP", SCREEN_WIDTH / 2, 4, 0);
-  writeText("Avaiable commands:", 0, 6, 0);
-  writeText("b - Go back", 0, 10, 0);
-  writeText("h - Open help screen", 0, 9, 0);
-  writeText("x - Stop the program", 0, 8, 0);
+  writeText("AJUDA", SCREEN_WIDTH / 2, 4, 0);
+  writeText("Comandos permitidos:", 0, 6, 0);
+  writeText("b - Voltar", 0, 10, 0);
+  writeText("h - Abre a tela de ajuda", 0, 9, 0);
+  writeText("x - Para o programa", 0, 8, 0);
+  writeText("0 - Em campos de codigo, cancela e volta para a tela anterior", 0, 11, 0);
   buildScreen();
 }
