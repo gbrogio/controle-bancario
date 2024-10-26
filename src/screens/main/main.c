e11cf8e07e904db12df037e41df4843477991fe5
Guilherme Brogio Macedo da Silva <guilhermebrogio.ps@gmail.com>
2024-10-26 11:59:47 -0300

    Refactor main screen text to match the app's purpose and language

--- src/screens/main/main.c
+++ src/screens/main/main.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include "../../global/global.h"
 #include "../../utils/screen/screen.h"
-#include "../../utils/cls/cls.h"
 
 void mainScreen() {
   cls();
-  writeText("APP DESCRIPTION", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 1, 0);
-  writeText("digit any key to execute the app.", SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) + 1, 0);
-  writeText("or press \"h\" to enter on help page.", SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) + 2, 0);
+  writeText("G.B.M.S. Bank - Trasferencias simples e rapidas", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 1, 0);
+  writeText("precione qualquer tecla para executar o app.", SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) + 1, 0);
+  writeText("ou precione \"h\" para entrar na tela de ajuda.", SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) + 2, 0);
   buildScreen();
 }
