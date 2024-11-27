4e3913e6e1d88c29ea917797cbab86f4f966a6b0
Guilherme Brogio Macedo da Silva <guilhermebrogio.ps@gmail.com>
2024-11-27 20:35:19 -0300

    formatando

--- src/screens/home.c
+++ src/screens/home.c
@@ -6,9 +6,9 @@ void homeScreen() {
   cls();
   writeText("G.B.M.S. Bank - Trasferencias simples e rapidas", SCREEN_WIDTH / 2,
             SCREEN_HEIGHT / 2 - 1, 0);
-  writeText("precione qualquer tecla para executar o app.", SCREEN_WIDTH / 2,
+  writeText("pressione qualquer tecla para executar o app.", SCREEN_WIDTH / 2,
             (SCREEN_HEIGHT / 2) + 1, 0);
-  writeText("ou precione \"h\" para entrar na tela de ajuda.", SCREEN_WIDTH / 2,
+  writeText("ou pressione \"h\" para entrar na tela de ajuda.", SCREEN_WIDTH / 2,
             (SCREEN_HEIGHT / 2) + 2, 0);
   buildScreen();
 }
